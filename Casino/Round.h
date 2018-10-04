e9d0a606b46413fc41928cef7d93e09f2751007a
ktavadze <ktavadze@gmail.com>
2018-10-04 14:37:11 -0400

    Refractors Round classs

--- Casino/Round.h
+++ Casino/Round.h
@@ -16,22 +16,22 @@ public:
         m_computer = a_computer;
         m_human = a_human;
 
-        m_human_next = Console::process_coin_toss();
-
         m_human->set_hand(m_deck.draw_set());
         m_computer->set_hand(m_deck.draw_set());
 
         m_table.set_loose_set(m_deck.draw_set());
+
+        m_human_next = Console::process_coin_toss();
     }
     Round(int a_number, Computer * a_computer, Human * a_human,
-        bool a_human_next, Table a_table, Deck a_deck)
+        Table a_table, Deck a_deck, bool a_human_next)
     {
         m_number = a_number;
         m_computer = a_computer;
         m_human = a_human;
-        m_human_next = a_human_next;
         m_table = a_table;
         m_deck = a_deck;
+        m_human_next = a_human_next;
     }
     void start();
     string ToString();
@@ -39,9 +39,9 @@ private:
     int m_number;
     Computer * m_computer;
     Human * m_human;
-    bool m_human_next;
     Table m_table;
     Deck m_deck;
+    bool m_human_next;
 
     bool is_over();
     void update_scores();
