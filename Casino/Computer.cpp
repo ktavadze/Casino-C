c42b858b20a026613c3ad7f4d0db5ff8cc805ce4
ktavadze <ktavadze@gmail.com>
2018-10-24 22:20:03 -0400

    Adds comments to Computer class

--- Casino/Computer.cpp
+++ Casino/Computer.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "Computer.h"
 
+/**********************************************************************
+Function Name: make_move
+Purpose: To allow the player (virtual) to make a move
+Parameters:
+    a_table, a Table instance passed by reference
+Return Value: The move code associated with made move, an integer value
+**********************************************************************/
 int Computer::make_move(Table & a_table)
 {
     if (can_increase(a_table))
@@ -28,6 +35,12 @@ int Computer::make_move(Table & a_table)
     return 0;
 }
 
+/**********************************************************************
+Function Name: process_increase_build
+Purpose: To allow the computer to increase the best existing build
+Parameters:
+    a_table, a Table instance passed by reference
+**********************************************************************/
 void Computer::process_increase_build(Table & a_table)
 {
     Build best_build = find_best_increase(a_table);
@@ -35,6 +48,12 @@ void Computer::process_increase_build(Table & a_table)
     increase_build(a_table, best_build);
 }
 
+/**********************************************************************
+Function Name: process_extend_build
+Purpose: To allow the computer to extend the best existing build
+Parameters:
+    a_table, a Table instance passed by reference
+**********************************************************************/
 void Computer::process_extend_build(Table & a_table)
 {
     Build best_build = find_best_extend(a_table);
@@ -42,6 +61,12 @@ void Computer::process_extend_build(Table & a_table)
     extend_build(a_table, best_build);
 }
 
+/**********************************************************************
+Function Name: process_extend_build
+Purpose: To allow the computer to create the best new build
+Parameters:
+    a_table, a Table instance passed by reference
+**********************************************************************/
 void Computer::process_create_build(Table & a_table)
 {
     Build best_build = find_best_create(a_table);
@@ -49,6 +74,12 @@ void Computer::process_create_build(Table & a_table)
     create_build(a_table, best_build);
 }
 
+/**********************************************************************
+Function Name: process_capture
+Purpose: To allow the computer to capture the best set
+Parameters:
+    a_table, a Table instance passed by reference
+**********************************************************************/
 void Computer::process_capture(Table & a_table)
 {
     Set best_capture_set = find_best_caputre(a_table);
@@ -56,6 +87,12 @@ void Computer::process_capture(Table & a_table)
     capture(a_table, best_capture_set);
 }
 
+/**********************************************************************
+Function Name: process_trail
+Purpose: To allow the computer to trail the best card
+Parameters:
+    a_table, a Table instance passed by reference
+**********************************************************************/
 void Computer::process_trail(Table & a_table)
 {
     Card best_trail_card = find_best_trail(a_table);
@@ -63,6 +100,13 @@ void Computer::process_trail(Table & a_table)
     trail(a_table, best_trail_card);
 }
 
+/**********************************************************************
+Function Name: increase_build
+Purpose: To allow the computer to increase specified build
+Parameters:
+    a_table, a Table instance passed by reference
+    a_build, a Build instance passed by value. Represents the increased build
+**********************************************************************/
 void Computer::increase_build(Table & a_table, Build a_build)
 {
     Set build_set = a_build.get_sets().at(0);
@@ -82,6 +126,13 @@ void Computer::increase_build(Table & a_table, Build a_build)
     }
 }
 
+/**********************************************************************
+Function Name: extend_build
+Purpose: To allow the computer to extend specified build
+Parameters:
+    a_table, a Table instance passed by reference
+    a_build, a Build instance passed by value. Represents the extended build
+**********************************************************************/
 void Computer::extend_build(Table & a_table, Build a_build)
 {
     Set build_set = a_build.get_sets().at(a_build.get_sets().size() - 1);
@@ -111,6 +162,13 @@ void Computer::extend_build(Table & a_table, Build a_build)
     }
 }
 
+/**********************************************************************
+Function Name: create_build
+Purpose: To allow the computer to create specified build
+Parameters:
+    a_table, a Table instance passed by reference
+    a_build, a Build instance passed by value. Represents the created build
+**********************************************************************/
 void Computer::create_build(Table & a_table, Build a_build)
 {
     Set build_set = a_build.get_sets().at(0);
@@ -131,6 +189,13 @@ void Computer::create_build(Table & a_table, Build a_build)
     a_table.add_build(a_build);
 }
 
+/**********************************************************************
+Function Name: capture
+Purpose: To allow the computer to capture specified set
+Parameters:
+    a_table, a Table instance passed by reference
+    a_capture_set, a Set instance passed by value
+**********************************************************************/
 void Computer::capture(Table & a_table, Set a_capture_set)
 {
     // Remove capture card from hand
@@ -158,6 +223,13 @@ void Computer::capture(Table & a_table, Set a_capture_set)
     }
 }
 
+/**********************************************************************
+Function Name: trail
+Purpose: To allow the computer to trail specified card
+Parameters:
+    a_table, a Table instance passed by reference
+    a_trail_card, a Card instance passed by value
+**********************************************************************/
 void Computer::trail(Table & a_table, Card a_trail_card)
 {
     // Remove trail card from hand
